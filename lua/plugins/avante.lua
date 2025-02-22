052a7880c66a364e96e4ea0f3b5bc09c61e7849c
J Lo <jlo@Jason-MBP-Personal.local>
2025-02-22 12:34:12 +0000

    feat: update

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,57 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  lazy = false,
+  version = false, -- set this if you want to always pull the latest change
+  opts = {
+    provider = "openai",
+    -- auto_suggestions_provider = "copilot",
+    openai = {
+      -- api_key_name = read_api_key(), -- the shell command must prefixed with `^cmd:(.*)`
+      -- api_key_name = {"bw","get","notes","anthropic-api-key"}, -- if it is a table of string, then default to command.
+    },
+    -- add any opts here
+  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = "make",
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false", -- for windows
+  dependencies = {
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below dependencies are optional,
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    -- "zbirenbaum/copilot.lua", -- for providers='copilot'
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+  file_selector = {
+    --- @alias FileSelectorProvider "native" | "fzf" | "mini.pick" | "snacks" | "telescope" | string
+    provider = "fzf",
+    -- Options override for custom providers
+    provider_opts = {},
+  },
+}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,66 @@
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local make_entry = require("telescope.make_entry")
+local conf = require("telescope.config").values
+
+local live_multigrep = function(opts)
+  opts = opts or {}
+  opts.cwd = opts.cwd or vim.uv.cwd()
+
+  local finder = finders.new_async_job({
+    command_generator = function(prompt)
+      if not prompt or prompt == "" then
+        return nil
+      end
+
+      local pieces = vim.split(prompt, "  ")
+      local args = { "rg" }
+
+      if pieces[1] then
+        table.insert(args, "-e")
+        table.insert(args, pieces[1])
+      end
+
+      if pieces[2] then
+        table.insert(args, "-g")
+        table.insert(args, pieces[2])
+      end
+
+      return vim
+        .iter({
+          args,
+          {
+            "--color=always",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+          },
+        })
+        :flatten()
+        :totable()
+    end,
+    entry_maker = make_entry.gen_from_vimgrep(opts),
+    cwd = opts.cwd,
+  })
+
+  pickers
+    .new(opts, {
+      debounce = 100,
+      prompt_title = "Multi Grep",
+      finder = finder,
+      previewer = conf.grep_previewer(opts),
+      sorter = require("telescope.sorters").empty(),
+      attach_mappings = function(_, map)
+        map("i", "<C-F>", require("telescope.actions").to_fuzzy_refine)
+        -- map("n", "<C-f>", actions.to_fuzzy_refine)
+        return true
+      end,
+    })
+    :find()
+end
+
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
@@ -37,6 +100,12 @@ return {
         })
       end,
       desc = "Lists files in your current working directory, respects .gitignore",
+    },
+
+    {
+      ";g",
+      live_multigrep,
+      desc = "Multi Grep",
     },
     {
       ";r",
